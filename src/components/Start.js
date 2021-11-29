224b1d0476fa7f959315e3b655cb42e8f0a0fe6f
Mateusz Drzał <mdrzal1886@gmail.com>
2021-11-29 21:31:34 +0100

    update README.md and fix start page scroll issue add Link buttons in start page on min-width 992px

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,9 +1,20 @@
 import logo from "../images/footballAppLogo.png";
+import englandFlag from "../images/englandFlag.png";
+import franceFlag from "../images/franceFlag.png";
+import { Link } from "react-router-dom";
 
 const Start = () => {
   return (
-    <main className="pageContainer">
+    <main className="pageContainer startContainer">
+      <Link to="/championship" className="leagueButton">
+        <h2>Championship</h2>
+        <img src={englandFlag} alt="england flag" />
+      </Link>
       <img src={logo} alt="logo" className="appLogo" />
+      <Link to="/ligue 2" className="leagueButton">
+        <h2>Ligue 2</h2>
+        <img src={franceFlag} alt="england flag" />
+      </Link>
     </main>
   );
 };
